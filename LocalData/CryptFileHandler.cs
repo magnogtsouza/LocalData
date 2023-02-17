60ab2cb6d0a4ee794e5c03aa9c04c5e789417beb
Magno Souza <magnogtsouza@outlook.com>
2023-02-17 20:21:24 -0300

    Adicionar arquivos de projeto.

---
+++ LocalData/CryptFileHandler.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LocalData
+{
+    internal class CryptFileHandler
+    {
+        public readonly string FilePath;
+        private byte[] _key;
+        private byte[] _iv;
+
+        public CryptFileHandler(string filePath, byte[] key, byte[] iv)
+        {
+            FilePath = filePath;
+            _key = key;
+            _iv = iv;
+        }
+
+        public CryptFileHandler(string filePath, string key, string iv)
+        {
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            byte[] ivBytes = Encoding.UTF8.GetBytes(iv);
+            if (keyBytes.Length != 32)
+            {
+                throw new NotImplementedException($"Internal encryption error, 32 character password required.");
+            }
+            if (ivBytes.Length != 16)
+            {
+                throw new NotImplementedException($"Internal encryption error, 16 character password required.");
+            }
+            if (keyBytes == null || ivBytes == null)
+            {
+                throw new NotImplementedException($"Internal encryption error: 0X0000");
+            }
+
+            _key = keyBytes;
+            _iv = ivBytes;
+            FilePath = filePath;
+        }
+
+        public void SaveString(string data)
+        {
+            SaveData(Encoding.UTF8.GetBytes(data));
+        }
+        private void SaveData(byte[] data)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = _key;
+                aes.IV = _iv;
+                aes.Mode = CipherMode.CBC;
+
+                using (FileStream fs = new FileStream(FilePath, FileMode.Create))
+                {
+                    using (CryptoStream cs = new CryptoStream(fs, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(data, 0, data.Length);
+                    }
+                }
+            }
+        }
+
+        public string LoadString()
+        {
+            byte[] data = LoadData();
+            return Encoding.UTF8.GetString(data);
+        }
+
+        private byte[] LoadData()
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = _key;
+                aes.IV = _iv;
+                aes.Mode = CipherMode.CBC;
+
+                using (FileStream fs = new FileStream(FilePath, FileMode.Open))
+                {
+                    using (CryptoStream cs = new CryptoStream(fs, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                    {
+                        byte[] data = new byte[fs.Length];
+                        cs.Read(data, 0, data.Length);
+                        return data;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LocalData/DataFile.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LocalData
+{
+    internal class DataFile
+    {
+        private static readonly CryptFileHandler MyEncryptedFile = new CryptFileHandler("date.bin", "A 32 CHARACTER PASSWORD !@#$%¨&*", "16CHARACTERPASSW");
+        private static readonly string VariablePrefix = "#54FV_";
+
+        public static void Clear() => System.IO.File.Delete(MyEncryptedFile.FilePath);
+
+        public static bool verifyExistCFG(string variable)
+        {          
+            variable = VariablePrefix + variable + "=";
+            if (!System.IO.File.Exists(MyEncryptedFile.FilePath))
+            {
+                return false;
+            }
+            string ss = MyEncryptedFile.LoadString();
+            if (!ss.Contains(variable))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static string loadCfg(string variable, string valueDefull = "")
+        {
+            variable = VariablePrefix + variable + "=";
+            if (!verifyExistCFG(variable.Replace(VariablePrefix, "").Replace("=", "").Trim()))
+            {
+                Save(variable.Replace(VariablePrefix, "").Replace("=", "").Trim(), valueDefull);
+                return valueDefull;
+            }
+            string[] MyCfg = MyEncryptedFile.LoadString().Replace("\0", "").Split('\n');
+            string resut = MyCfg.ToList().FirstOrDefault(x => x.Contains(variable)).Split('=')[1];
+            return resut;
+        }
+
+        public static bool loadCfg(string variable, bool valueDefull)
+        {
+            string res = loadCfg(variable, valueDefull ? "1" : "0");
+            return res.Contains('1');
+        }
+        public static int loadCfg(string variable, int valueDefull)
+        {
+            try
+            {
+                string res = loadCfg(variable, valueDefull.ToString());
+                return Convert.ToInt32(res);
+            }
+            catch
+            {
+                return valueDefull;
+            }
+        }
+
+        private static string GetVariableKey(string variable)
+        {
+            return variable = VariablePrefix + variable + "=";
+        }
+        public static bool Save(string variable, string value)
+        {
+            string variableKey = GetVariableKey(variable);
+
+            if (!System.IO.File.Exists(MyEncryptedFile.FilePath))
+            {
+                MyEncryptedFile.SaveString(variableKey + "=" + value);
+                return true;
+            }
+
+            string[] lines = MyEncryptedFile.LoadString().Split('\n');
+            int index = Array.FindIndex(lines, line => line.StartsWith(variableKey));
+
+            if (index == -1)
+            {
+                MyEncryptedFile.SaveString(variableKey + "=" + value + "\n" + MyEncryptedFile.LoadString());
+                return true;
+            }
+
+            lines[index] = variableKey + "=" + value;
+            MyEncryptedFile.SaveString(string.Join("\n", lines));
+            return true;
+        }
+
+        public static bool Save(string variable, bool value) => Save(variable, value ? "1" : "0");
+
+        public static bool Save(string variable, int value) => Save(variable, value.ToString());
+    }
+}
